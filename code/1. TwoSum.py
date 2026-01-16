e6454552a07c0df3986b6e04e7e267b09f7ccb1d
Akanksha Pandey <aakanksha.vit@gmail.com>
2026-01-16 00:12:30 -0500

    updated TwoSum

--- code/1. TwoSum.py
+++ code/1. TwoSum.py
@@ -1,5 +1,17 @@
 from typing import List
 
+class Test:
+    def __init__(self, nums : List[int], target : int, expected : List[int]):
+        self.nums = nums
+        self.target = target
+        self.expected = expected
+    
+    def ToString(self) -> str:
+        return "nums: [" + ", ".join(str(n) for n in self.nums) + "], target: "+ str(self.target)
+
+    def IsCorrect(self, answer : List[int]) -> bool:
+        return sorted(answer) == sorted(self.expected)
+
 def RunTests(testcases) -> None:
     s = Solution()
     i = 1
@@ -11,20 +23,16 @@ def RunTests(testcases) -> None:
             print(f"Test #{i} FAILED!")
             print(f"\t{test.ToString()}, Solution : [{answer[0]},{answer[1]}] should be {test.expected}")
         i+=1
-class Test:
-    def __init__(self, nums : List[int], target : int, expected : List[int]):
-        self.nums = nums
-        self.target = target
-        self.expected = expected
-    
-    def ToString(self):
-        text = ""
-        for num in self.nums:
-            text = text + str(num) + ", "
-        return "nums: [" + text[:-2] + "], target: "+ str(self.target)
-    
-    def IsCorrect(self, answer : List[int]) -> bool:
-        return sorted(answer) == sorted(self.expected)
+
+###################################################################
+# Solution class and main function
+###################################################################
+
+testcases = [ 
+    Test([2,7,11,15], 9, [0, 1]), 
+    Test([3,2,4], 6, [1, 2]), 
+    Test([3,3], 6, [0, 1])
+]    
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mapNumToIndex = {}
@@ -35,11 +43,6 @@ class Solution:
         return []
 
 def main():
-    testcases = [ 
-        Test([2,7,11,15], 9, [0, 1]), 
-        Test([3,2,4], 6, [1, 2]), 
-        Test([3,3], 6, [0, 1])
-        ]
     RunTests(testcases)
         
 if __name__ == "__main__":
